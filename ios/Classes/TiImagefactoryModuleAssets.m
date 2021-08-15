72aa182492aabde2cffbe03d09bb1008d5a359f0
Hans Knöchel <hans@MacBook-Air-von-Hans.fritz.box>
2021-08-15 15:58:32 +0200

    feat: support macOS catalyst

--- ios/Classes/TiImagefactoryModuleAssets.m
+++ ios/Classes/TiImagefactoryModuleAssets.m
@@ -3,18 +3,20 @@
  */
 #import "TiImagefactoryModuleAssets.h"
 
-extern NSData *filterDataInRange(NSData *thedata, NSRange range);
+extern NSData* filterDataInRange(NSData* thedata, NSRange range);
 
 @implementation TiImagefactoryModuleAssets
 
 - (NSData *)moduleAsset
 {
+  
 
   return nil;
 }
 
 - (NSData *)resolveModuleAsset:(NSString *)path
 {
+  
 
   return nil;
 }
